c656dc798e148de310be619eb90a6471b5ff780d
Gnurgen <gnurgen@gmail.com>
2015-11-24 15:00:07 +0100

    Added all the files.

---
+++ src/com/acertainbookstore/utils/BookStoreMessageTag.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.acertainbookstore.utils;
+
+/**
+ * BookStoreMessageTag implements the messages supported in the bookstore
+ * 
+ */
+public enum BookStoreMessageTag {
+	ADDBOOKS, 
+	LISTBOOKS, 
+	ADDCOPIES, 
+	GETBOOKS, 
+	BUYBOOKS, 
+	UPDATEEDITORPICKS, 
+	EDITORPICKS, 
+	REMOVEALLBOOKS, 
+	REMOVEBOOKS, 
+	GETSTOCKBOOKSBYISBN, 
+	GETTOPRATEDBOOKS, 
+	GETBOOKSINDEMAND, 
+	RATEBOOKS;
+}
